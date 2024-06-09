cf9f28983fd156300e409be2a7a048ac214fc144
saraanasim <saraanofficial@gmail.com>
2024-06-09 15:31:50 +0500

    fix(db): Fixed db connection
    Fixed bug and injected config service in database module
    Set mongodb uri in env

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -7,7 +7,8 @@ import { MongooseModule } from '@nestjs/mongoose';
         MongooseModule.forRootAsync({
             useFactory:(configService:ConfigService)=>({
                 uri:configService.get<string>('MONGODB_URI')
-            })
+            }),
+            inject: [ConfigService],
         })
     ],
 })
